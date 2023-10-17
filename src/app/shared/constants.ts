f7a825ed104de97986be1e2e606a5e8ffa2fa013
Omar Yazzaoui <omar.yazzaoui1@ibm.com>
2023-10-17 21:15:35 +0100

    Auto-generated commit [ci skip]

--- src/app/shared/constants.ts
+++
@@ -1,7 +0,0 @@
-export const Countries = {
-  england: 39,
-  spain: 140,
-  france: 61,
-  germany: 78,
-  italy: 135,
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  apiUrl: 'https://v3.football.api-sports.io',
-  apiKey: '26d2d0d2053bebbac05c1a0803a9bd29',
-};
